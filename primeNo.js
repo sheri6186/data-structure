bcaef52909101a7a83961edd54d7e7562e0c0c39
sheri6186 <sheri.6186@gmail.com>
2024-08-27 15:49:06 +0500

    optimality test check for prime no and their time complexity

--- primeNo.js
+++ primeNo.js
@@ -2,7 +2,7 @@ function prime(n){
     if(n<2){
         return true
     }
-    for(let i = 2;i<n;i++){
+    for(let i = 2;i<Math.sqrt(n);i++){
         if(n%i==0){
             return false
         }
